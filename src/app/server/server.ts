c719287f57554b9d0abbee1fd6d3b5d37a46b2fe
Marcio Luis da Silva <marcio.luis.da.silva@accenture.com>
2017-10-02 16:14:03 -0300

    removeing swagger_express

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-var swagger = require('swagger-express');
 import { Application, Router, Request, Response } from 'express';
 
 import * as routes from "../routes";
@@ -18,17 +17,6 @@ export class Server {
 
     public init() {
         this.app = express();
-        this.app.use(swagger.init(this.app, {
-            apiVersion: '1.0',
-            swaggerVersion: '1.0',
-            swaggerURL: '/swagger',
-            swaggerJSON: '/api-docs.json',
-            swaggerUI: './public/swagger/',
-            basePath: 'http://localhost:3002',
-            apis: [],
-            middleware: function(req, res){}
-          }));
-
         this.app.listen(this.config.port, () => {
             console.log(`Listening at http://localhost:` + this.config.port);
         });
